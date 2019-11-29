84af67b395ba25748face07d597deb0bb9d1ff6c
Ahmet Inan <inan@aicodix.de>
2019-11-29 23:00:22 +0100

    avoid inferring of multiple adders

--- add_vector.vhd
+++ add_vector.vhd
@@ -19,20 +19,26 @@ end add_vector;
 architecture rtl of add_vector is
 	function add (val : soft_vector; sgn : sgn_vector; mag : mag_vector) return soft_vector is
 		variable tmp : soft_vector;
+		subtype signed_mag_scalar is integer range -mag_scalar'high to mag_scalar'high;
+		type signed_mag_vector is array (0 to vector_scalars-1) of signed_mag_scalar;
+		variable sig : signed_mag_vector;
+		subtype sum_scalar is integer range soft_scalar'low-mag_scalar'high to soft_scalar'high+mag_scalar'high;
+		type sum_vector is array (0 to vector_scalars-1) of sum_scalar;
+		variable sum : sum_vector;
 	begin
 		for idx in tmp'range loop
 			if sgn(idx) then
-				if val(idx)-mag(idx) < soft_scalar'low then
-					tmp(idx) := soft_scalar'low;
-				else
-					tmp(idx) := val(idx)-mag(idx);
-				end if;
+				sig(idx) := -mag(idx);
 			else
-				if val(idx)+mag(idx) > soft_scalar'high then
-					tmp(idx) := soft_scalar'high;
-				else
-					tmp(idx) := val(idx)+mag(idx);
-				end if;
+				sig(idx) := mag(idx);
+			end if;
+			sum(idx) := val(idx) + sig(idx);
+			if sum(idx) > soft_scalar'high then
+				tmp(idx) := soft_scalar'high;
+			elsif sum(idx) < soft_scalar'low then
+				tmp(idx) := soft_scalar'low;
+			else
+				tmp(idx) := sum(idx);
 			end if;
 		end loop;
 		return tmp;
